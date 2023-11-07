b41711764d3a8c529bb097aef85efde3a8792ee3
AnthonyKeung <anthonykeung2468@gmail.com>
2023-11-07 16:04:40 +0000

    Working Temperature class can read and get the temperature

--- main.cpp
+++ main.cpp
@@ -1,7 +1,49 @@
-
 #include "mbed.h"
-#include "TemperatureSensor.h"
 
+class TemperatureSensor
+{
+    private:
+    int desiredTemperature;
+    int currentTemperature;
+    const int TemperatureSensorAddress = 0x9A;
+    bool debug_mode = true;
+
+    public:
+    TemperatureSensor(int desiredTemperature);
+    bool checkSensorConnected();
+    void read();
+    int getTemperatureReading();
+};
+
+TemperatureSensor::TemperatureSensor(int desiredTemperature):
+    desiredTemperature(desiredTemperature)
+    {
+    };
+
+void TemperatureSensor::read()
+{
+    I2C i2c(PB_9, PB_8);
+    char data[2];
+    i2c.read(TemperatureSensorAddress, data, 1); 
+    currentTemperature = data[0];
+    wait_us(100000); 
+}
+
+int TemperatureSensor::getTemperatureReading()
+{
+    return currentTemperature;
+}
+
+bool TemperatureSensor::checkSensorConnected()
+{
+    I2C i2c(PB_9, PB_8);
+    if (!i2c.write(TemperatureSensorAddress, NULL, 0)) 
+    { // 0 returned is ok
+        return true;
+    }
+    return false;
+
+}
 
 enum Mode
 {
@@ -30,18 +72,36 @@ void BUTTONINTERUPT()
 }
 
 
-
 int main()
 {
+    //Printing setup 
+    BufferedSerial mypc(USBTX, USBRX);
+    FILE* mypcFile1 = fdopen(&mypc, "r+");
+
     led1 = 0;
     led2 = 0;
+
+    //Interrupt setups
     button.rise(&BUTTONINTERUPT);
-    TempSense.checkSensorConnected();
+
+    if (TempSense.checkSensorConnected())
+    {
+        fprintf(mypcFile1, "The sensor is succesfully connected \n");
+        wait_us(1000000);
+    }
+    else
+    {
+        fprintf(mypcFile1, "The sensor is succesfully connected \n");
+        wait_us(1000000);
+    }
+
+    wait_us(1000000);
+
     while (true) 
     {
         TempSense.read();
-        printf("The current Temperature is %d" ,TempSense.getTemperatureReading());
-        printf("The mode is %d \n" , mode);
+        fprintf(mypcFile1,"The current Temperature is %d \n" ,TempSense.getTemperatureReading());
+
         if (mode == CLOSEDLOOP)
         {
             led1 = 1;
@@ -56,6 +116,10 @@ int main()
         {
             led1 = 1;
             led2 = 1;
+        }
+        else 
+        {
+            printf("Should never reach here");
         }
     }
 }
