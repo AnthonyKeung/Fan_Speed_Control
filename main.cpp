c8909e52f1ddd0026057f4dcd0ed349e6a4f7705
Patryk Kuzma <kupa2812@gmail.com>
2023-12-07 14:01:50 +0000

    Adds PID for speed control.

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ BusOut leds(LED1,LED2);
 TemperatureSensor TempSense(24);
 TextLCD lcd(PB_15, PB_14, PB_10, PA_8, PB_2, PB_1);
 PID controller(10, 0, 0, PIDRATE);
+PID speed(0.1, 0.000, 0, PIDRATE);
 bool enc_rotated = false;      // rotary encoder was rotated left or right
 int pulseCount;
 
@@ -35,6 +36,14 @@ int main()
     enable_Button();
     enableRotaryEncoder();
     setPeriodms(5);
+
+    //pwm*100 input from 0.0 to 3.3V
+    speed.setInputLimits(0, 1950);
+    //Pwm output from 0.0 to 1.0
+    speed.setOutputLimits(0, 1);
+    //If there's a bias.
+    speed.setBias(0);
+    speed.setMode(1);
 
     //Analog input from 0.0 to 3.3V
     controller.setInputLimits(0, 40);
@@ -75,6 +84,21 @@ int main()
             printf ("RPM:  %i\n\r" , RPMcalculate());
             lcd.cls();
             lcd.printf("S= %d", RPMread());
+        }
+        else if (getMode() == SLOWCONTROL)
+        {   
+            //PID Calculations
+            speed.setSetPoint(getPulseCount()*19.5);
+            speed.setProcessValue(RPMcalculate());
+            setFan(speed.compute());
+            //Display the values 
+            lcd.cls();
+            lcd.printf("T = %d ", getPulseCount());
+            lcd.locate(0, 1);
+            lcd.printf("S= %d", RPMread());
+            printf("Set Point: %i  RPM: %i  PWM value %.3f\n\r" ,int(getPulseCount()*19.5),RPMread(), speed.compute());
+
+            //fprintf(mypcFile1,"The current Temperature is %d \n" ,TempSense.getTemperatureReading());
         }
 
         // shaft has been rotated?
